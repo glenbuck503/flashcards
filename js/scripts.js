3ae27c1bb15e80afa7bfbe7e75f60d9c73850b85
glenbuck503 <glenbuck503@gmail.com>
2020-10-20 18:57:29 -0700

    add and hide definitions

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,58 @@
+
+
+
 $(document).ready(function() {
-  $(".clickable").click(function() {
+  $(".clickJava").click(function() {
     $("#javascript-showing").toggle();
     $("#javascript-hidden").toggle();
-    
   });
 });
+
+$(document).ready(function() {
+  $(".clickOp").click(function() {
+    $("#operators-showing").toggle();
+    $("#operators-hidden").toggle();
+  });
+});
+
+
+$(document).ready(function() {
+  $(".clickVar").click(function() {
+    $("#variables-showing").toggle();
+    $("#variables-hidden").toggle();
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $(document).ready(function() {
+//   $("button#java").click(function(){
+//     $("#javascript-showing").toggle();
+//     $("#javascript-hidden").toggle();
+
+//     });
+//   });
+
+//   $(document).ready(function() {
+//     $("button#operators").click(function(){
+//       $("#oeprators-showing").toggle();
+//       $("#operators-hidden").toggle();
+  
+//       });
+//     });
+
